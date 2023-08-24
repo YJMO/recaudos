2dec2d2f7ce95b14c513e5018740201bf845b378
YJMO <yeinerjmarquez@gmail.com>
2023-08-24 18:57:26 -0500

    Initial commit

--- app/src/main/java/com/example/recaudos/MainActivity.kt
+++ app/src/main/java/com/example/recaudos/MainActivity.kt
@@ -1,47 +1,42 @@
 package com.example.recaudos
-
+import android.media.RingtoneManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
-import android.widget.DatePicker
-import android.widget.EditText
-import android.widget.ImageButton
+import android.widget.*
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    var txtFecha:EditText?=null
+    var btnFecha:ImageButton?=null
+    var dpFecha:DatePicker?=null
 
-    var txtfecha:EditText?=null
-    var botonfecha:ImageButton?=null
-    var dpfecha:DatePicker?=null
-
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txtfecha= findViewById(R.id.txtfecha)
-        botonfecha = findViewById(R.id.botonfecha)
-        dpfecha = findViewById(R.id.dpfecha)
+        txtFecha=findViewById(R.id.txtFecha)
+        btnFecha=findViewById(R.id.btnFecha)
+        dpFecha=findViewById(R.id.dpFecha)
 
-        txtfecha?.setText(getFechaDatePicker())
+        txtFecha?.setText(getFechaDtePicker())
 
-        dpfecha?.setOnDateChangedListener{
-            dpfecha,anho,mes,dia->
-            txtfecha?.setText(getFechaDatePicker())
-            dpfecha.visibility=View.GONE
+        dpFecha?.setOnDateChangedListener{
+                dpFecha,anio,mes,dia->
+            txtFecha?.setText(getFechaDtePicker())
+            dpFecha?.visibility=View.GONE
         }
-
     }
-
-    fun getFechaDatePicker():String{
-        var dia = dpfecha?.dayOfMonth.toString().padStart(length = 2, padChar = '0')
-        var mes = (dpfecha!!.month+1).toString().padStart(length = 2, padChar = '0')
-        var anho = dpfecha?.year.toString().padStart(length = 4, padChar = '0')
-
-        return  dia+"/"+mes+"/"+anho
+    fun getFechaDtePicker():String{
+        var dia=dpFecha?.dayOfMonth.toString().padStart(2,'0')
+        var mes=(dpFecha!!.month+1).toString().padStart(2,'0')
+        var anio=dpFecha?.year.toString().padStart(4,'0')
+        return dia+"/"+mes+"/"+anio
     }
-
-    fun mostrarcalendario(view: View){
-        botonfecha?.visibility=View.VISIBLE
+    fun muestraCalendario(view: View){
+        dpFecha?.visibility=View.VISIBLE
     }
 }
-
-https://www.youtube.com/watch?v=WJCy_1RDFbg&ab_channel=ProgramadorNovato
